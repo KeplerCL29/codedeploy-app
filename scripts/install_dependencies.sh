566f0aa490d2cf9e5ebaece4e04d1fb14f102eef
KeplerCL29 <KeplerCL29@uww.edu>
2025-05-10 20:49:56 -0500

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,8 +4,6 @@ set -e
 echo "Updating package lists…"
 sudo apt-get update -y
 
-echo "Installing Ruby, wget, Python3, and pip…"
-sudo apt-get install -y ruby wget python3 python3-pip
+echo "Installing Apache, Ruby, wget, Python3…"
+sudo apt-get install -y apache2 ruby wget python3 python3-pip
 
-# any other dependencies you need, e.g.
-# sudo apt-get install -y build-essential
